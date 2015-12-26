b65012477bb81a4215859bdf914b07c5812e9b4c
Andrew Rabert <draje@nullsum.net>
2015-12-26 12:27:05 -0500

    updated servers

--- settings.py
+++ settings.py
@@ -9,21 +9,38 @@ openvpn_config_file = 'config'
 protocol_ports = {'udp': [53,1194,8080,9201]
                  ,'tcp': [80,110,443]}
 
-servers = {'CA North York': 'ca.privateinternetaccess.com' 
-          ,'CA Toronto': 'ca-toronto.privateinternetaccess.com' 
-          ,'France': 'france.privateinternetaccess.com' 
-          ,'Germany': 'germany.privateinternetaccess.com'
-          ,'Hong Kong': 'hk.privateinternetaccess.com'
-          ,'Netherlands': 'nl.privateinternetaccess.com'
-          ,'Romania': 'ro.privateinternetaccess.com'
-          ,'Sweden': 'sweden.privateinternetaccess.com'
-          ,'Switzlerand': 'swiss.privateinternetaccess.com'
-          ,'UK London': 'uk-london.privateinternetaccess.com'
-          ,'UK Southampton': 'uk-southampton.privateinternetaccess.com'
-          ,'US California': 'us-california.privateinternetaccess.com'
-          ,'US East': 'us-east.privateinternetaccess.com'
-          ,'US Florida': 'us-florida.privateinternetaccess.com'
-          ,'US Midwest': 'us-midwest.privateinternetaccess.com'
-          ,'US Seattle': 'us-seattle.privateinternetaccess.com'
-          ,'US Texas': 'us-texas.privateinternetaccess.com'
-          ,'US West': 'us-west.privateinternetaccess.com'}
+servers = {'AU Melbourne': 'aus-melbourne.privateinternetaccess.com',
+           'AU Sydney': 'aus.privateinternetaccess.com',
+           'Brazil': 'brazil.privateinternetaccess.com',
+           'CA North York': 'ca.privateinternetaccess.com',
+           'CA Toronto': 'ca-toronto.privateinternetaccess.com',
+           'Denmark': 'denmark.privateinternetaccess.com',
+           'France': 'france.privateinternetaccess.com',
+           'Germany': 'germany.privateinternetaccess.com',
+           'Hong Kong': 'hk.privateinternetaccess.com',
+           'India': 'in.privateinternetaccess.com',
+           'Ireland': 'ireland.privateinternetaccess.com',
+           'Israel': 'israel.privateinternetaccess.com',
+           'Italy': 'italy.privateinternetaccess.com',
+           'Japan': 'japan.privateinternetaccess.com',
+           'Mexico': 'mexico.privateinternetaccess.com',
+           'Netherlands': 'nl.privateinternetaccess.com',
+           'New Zealand': 'nz.privateinternetaccess.com',
+           'Norway': 'no.privateinternetaccess.com',
+           'Romania': 'ro.privateinternetaccess.com',
+           'Russia': 'russia.privateinternetaccess.com',
+           'Singapore': 'sg.privateinternetaccess.com',
+           'Sweden': 'sweden.privateinternetaccess.com',
+           'Switzerland': 'swiss.privateinternetaccess.com',
+           'Turkey': 'turkey.privateinternetaccess.com',
+           'UK London': 'uk-london.privateinternetaccess.com',
+           'UK Southampton': 'uk-southampton.privateinternetaccess.com',
+           'US California': 'us-california.privateinternetaccess.com',
+           'US East': 'us-east.privateinternetaccess.com',
+           'US Florida': 'us-florida.privateinternetaccess.com',
+           'US Midwest': 'us-midwest.privateinternetaccess.com',
+           'US New York City': 'us-newyorkcity.privateinternetaccess.com',
+           'US Seattle': 'us-seattle.privateinternetaccess.com',
+           'US Silicon Valley': 'us-siliconvalley.privateinternetaccess.com',
+           'US Texas': 'us-texas.privateinternetaccess.com',
+           'US West': 'us-west.privateinternetaccess.com'}
